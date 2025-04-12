efe5881bda2cca43667371e4e48e3a30905f045c
Joram Jesse <joramjessebakinikana@gmail.com>
2025-04-12 17:34:54 +0300

    -Refactored: file locations to keep the hierarchy consistent -Refactored: model_api.py to inference_pipeline.py -Removed: notebook.py, and other deprecated files and folders

--- source/classes/PreliminaryCleaner.py
+++ source/classes/PreliminaryCleaner.py
@@ -8,13 +8,11 @@ class PreliminaryCleaner:
 
     def clean_data(self) -> pd.DataFrame:
         """Cleans the dataset by removing unnecessary columns and normalizing text"""
-
-        self.df = self.df.drop(columns=['job_id', 'location', 'department',
-                                        'telecommuting', 'has_company_logo', 'has_questions'])
-
         self.df['nature_of_job'] = self.df['title'] + ' ' + self.df['function']
         self.df['company_profile_and_description'] = self.df['company_profile'] + ' ' + self.df['description']
-        self.df.drop(columns=['title', 'function', 'company_profile', 'description'], inplace=True)
+
+        # self.df.drop(columns=['title', 'function', 'company_profile', 'description', 'job_id', 'location', 'department',
+        #                       'telecommuting', 'has_company_logo', 'has_questions'], inplace=True)
 
         self.df.rename(columns={'industry': 'nature_of_company',
                                 'required_experience': 'type_of_position',
@@ -24,4 +22,3 @@ class PreliminaryCleaner:
             lambda x: x.str.lower())
 
         return self.df
-
